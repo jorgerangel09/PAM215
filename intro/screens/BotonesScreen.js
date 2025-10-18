9d29037bca6eca240ab6a4bd652a8fb898833447
jorgerangel09 <124048757@upq.edu.mx>
2025-10-17 20:15:30 -0600

    Practica 8 Button&Switch

--- intro/screens/BotonesScreen.js
+++ intro/screens/BotonesScreen.js
@@ -1,11 +1,70 @@
-import { Text, StyleSheet, View, Button } from 'react-native'
+import { Text, StyleSheet, View, Button, Switch } from 'react-native'
+import React,{useState} from 'react';
 
 export default function BotonesScreen(){
+  const [esEncendido, cambiarEncendido] = useState(false);
+  const [color, cambiarColor] = useState('yellow');
+
     return (
-      <View>
-        <Text>Proximamente</Text>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Control de luz</Text>
+        
+        {/* Operador terniario 
+        {condicional ? valorTrue : valorFalse}
+        */}
+        <Text style={[styles.luz, {color: esEncendido ? color:'white'}]}>
+          {esEncendido ? 'Luz Encendida' : 'Luz Apagada'}
+        </Text>
+
+        <Switch style={{marginBottom:10}}
+        value={esEncendido}
+        onValueChange = {() => cambiarEncendido(!esEncendido)}
+        trackColor = {{true: '#03b40fff', false: 'grey'}}
+        ></Switch>
+
+      <View style={styles.cajaBotones}>
+        <Button
+        title='Amarillo'
+        onPress={() =>esEncendido && cambiarColor('yellow')}
+        color='#aaaa16ff'
+        ></Button>
+
+        <Button
+        title='Azul'
+        onPress={() =>esEncendido && cambiarColor('blue')}
+        color='#2525adff'
+        ></Button>
+
+        <Button
+        title='Rojo'
+        onPress={() =>esEncendido && cambiarColor('red')}
+        color='#ad2525ff'
+        ></Button>
+      </View>
       </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+   container: {
+    flex: 1,
+    backgroundColor: '#000000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cajaBotones:{
+    flexDirection:'column',
+    gap:10,
+    mrginTop:15,
+  },
+  titulo:{
+    fontSize:40,
+    color:'#8200c8ff',
+    marginBottom:20,
+    fontWeight: 'bold',
+  },
+  luz:{
+    fontSize:30,
+    marginBottom:20,
+  }
+})
